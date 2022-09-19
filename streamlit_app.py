65139eb59d5da6fad84cc96db0262379b0964b4e
Kshitiz <113875663+Kshitiz678@users.noreply.github.com>
2022-09-19 16:51:21 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit
 
-streamlit.title('Hi there')
-streamlit.header('this is header')
-streamlit.text('This is just text')
+streamlit.title('Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
