b2af6313e087728954014f3685968b9e909d6585
Kshitiz <113875663+Kshitiz678@users.noreply.github.com>
2022-09-22 15:22:07 +1200

    Update streamlit_app.py
    import snowflake.connector

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,8 +22,6 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-
-
 import requests
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
@@ -33,3 +31,4 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # call fruityvice datafareme
 streamlit.dataframe(fruityvice_normalized)
 
+import snowflake.connector
